e81a61b510ced39a04577509035072d317250b7e
Ayush Sagar <ayushsagar3018@gmail.com>
2025-09-23 10:51:32 -0400

    added background gradient

--- project-one/project-one/ContentView.swift
+++ project-one/project-one/ContentView.swift
@@ -8,20 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sliderValue: Double = 0
-    private var songLength: Int = 101
+    @State private var sliderValue: Double = 0;
+    private let songLength: Int = 101;
+    
+    
     var body: some View {
         VStack {
+            Spacer()
             HStack {
-                Image(systemName: "chevron.down")
+                Image(systemName: "chevron.down").font(.largeTitle)
                 Spacer()
-                Text("Logic")
+                Text("Logic").font(.title2).bold()
                 Spacer()
-                Image(systemName: "ellipsis")
+                Image(systemName: "ellipsis").font(.title2)
             }
             Image("no-pressure")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
             VStack {
                 HStack {
                     VStack (alignment: .leading) {
@@ -33,18 +37,39 @@ struct ContentView: View {
                 }
                 Slider(
                     value: $sliderValue,
-                    in: 0...60,
+                    in: 0...140,
                     step: 1.0
-                )
+                ).accentColor(.gray)
                 HStack {
-                    Text("0").font(.caption)
+                    Text("0:01").font(.caption)
                     Spacer()
-                    Text("60").font(.caption)
+                    Text("-1.39").font(.caption)
+                }
+                HStack {
+                    Image(systemName: "shuffle").font(.title2)
+                    Spacer()
+                    Image(systemName: "backward.end.fill").font(.system(size: 50))
+                    Spacer()
+                    Image(systemName: "pause.circle.fill").font(.system(size: 75))
+                    Spacer()
+                    Image(systemName: "forward.end.fill").font(.system(size: 50))
+                    Spacer()
+                    Image(systemName: "repeat").font(.title2)
+                }.padding(.bottom)
+                HStack {
+                    Image(systemName: "tv.and.hifispeaker.fill")
+                    Image(systemName: "square.and.arrow.up")
+                    
                 }
                 
             }
+            Spacer()
         }
-        .padding()
+        .foregroundStyle(.white)
+        .padding(.horizontal)
+        .background(
+            LinearGradient(gradient: Gradient(colors: [.orange, .red]), startPoint: .topLeading, endPoint: .bottomTrailing)
+        )
     }
 }
 
